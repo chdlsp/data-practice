0bc4fc1bc9e4f7fc2f3336009b6b349e6c424acf
Chris <44944612+chdlsp@users.noreply.github.com>
2020-09-23 19:05:02 +0900

    Initial commit

---
+++ src/main/java/com/chdlsp/datapractice/DataPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.chdlsp.datapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPracticeApplication.class, args);
+	}
+
+}
